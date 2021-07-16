bed6fd016cda0590332d6a2537472258cf1122b4
GY Noh <nohmad@gmail.com>
2021-07-16 18:57:09 +0900

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+AWS_CONFIG=/root/.aws/config
+
+if [ ! -f "$AWS_CONFIG" ]; then 
+  cat <<EOF > $AWS_CONFIG
+[default]
+region = ${AWS_REGION}
+cli_binary_format = raw-in-base64-out
+EOF
+fi
+
+TEMP=$(mktemp)
+sh -c "aws $*" > $TEMP
+if [ -z "$JQ_ARGS" ]; then
+  echo "::set-output name=result::$(cat ${TEMP})"
+else
+  echo "::set-output name=result::$(cat ${TEMP} | jq $JQ_ARGS)"
+fi
